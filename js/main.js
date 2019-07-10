b91b2200eb128cfde8789129eb8d29e55673c058
4ubaka447 <y.n.semenchenko@mail.ru>
2019-07-10 16:40:09 +0300

    Реализует наложение эффектов

--- js/main.js
+++ js/main.js
@@ -170,7 +170,7 @@ var photoDescriptions = getPhotoDescription(PHOTO_DESCRIPTION);
 insertRandomUserImges(photoDescriptions, randomUserImgContainer);
 
 
-// module4-task1
+// module4-task1 + module5-task1
 
 var ESC_KEYCODE = 27;
 var UPLOADED_IMG_DEFAULT_CLASS = 'img-upload__preview';
@@ -207,84 +207,6 @@ uploadFile.addEventListener('change', openPopup);
 uploadCancel.addEventListener('click', closePopup);
 
 
-// реализация насыщенности эффекта
-
-var effectLevel = imgUpload.querySelector('.effect-level');
-var effectLevelPin = effectLevel.querySelector('.effect-level__pin');
-var effectLevelDepth = effectLevel.querySelector('.effect-level__depth');
-var effectLevelLine = effectLevel.querySelector('.effect-level__line');
-// var effectLevelValue = effectLevel.querySelector('.effect-level__value');
-
-// функция сброса уровня эффекта, на уровень по умолчанию
-var resetEffectLevel = function (defaultEffectLevel) {
-  effectLevelPin.style.left = defaultEffectLevel + '%';
-  effectLevelDepth.style.width = defaultEffectLevel + '%';
-};
-
-resetEffectLevel(DEFAULT_EFFECT_LEVEL);
-
-
-effectLevelPin.addEventListener('mousedown', function (evt) {
-  // все перемещения слайдера будем считать в процентах,
-  // для этого найдем масштбаный коэффициент
-  // получим ширину слайдера и соотнесем ее к 100%
-  var scaleFactor = effectLevelLine.offsetWidth / 100; // получаем ширину
-  var startX = evt.clientX;
-  var onMouseMove = function (moveEvt) {
-
-    var shift = (startX - moveEvt.clientX);
-    startX = moveEvt.clientX;
-    var currentPosition = effectLevelPin.offsetLeft;
-    var sliderPosition = (currentPosition - shift) / scaleFactor;
-
-    if (sliderPosition < 0) {
-      sliderPosition = 0;
-    }
-
-    if (sliderPosition > 100) {
-      sliderPosition = 100;
-    }
-
-    effectLevelPin.style.left = sliderPosition + '%';
-    effectLevelDepth.style.width = sliderPosition + '%';
-  };
-
-  var onMouseUp = function () {
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
-  };
-
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
-});
-
-// реализация смены эффектов
-
-var imgUploadPreview = imgUpload.querySelector('.img-upload__preview');
-var effectsList = imgUpload.querySelector('.effects__list');
-
-// функция добавляет целевому элементу задаваемый класс
-// elemTarget - целевой элемент
-// extraClass - задаваемый класс
-var addElemntEffect = function (elemTarget, extraClass) { // сюда строку
-  elemTarget.classList = ''; // обнуляем класс лист
-  elemTarget.classList.add(UPLOADED_IMG_DEFAULT_CLASS); // добавляем класс по умолчанию
-  elemTarget.classList.add(extraClass); // добавляем заданный класс
-};
-
-// функция обработчик
-var onEffectsItemClick = function (evt) {
-
-  if (evt.target.classList.contains('effects__preview')) {
-    var extraClass = evt.target.classList.item(1); // забираем нужный класс у элемента по которому кликаем
-    addElemntEffect(imgUploadPreview, extraClass);
-    resetEffectLevel(DEFAULT_EFFECT_LEVEL);
-  }
-};
-
-effectsList.addEventListener('click', onEffectsItemClick);
-
-
 // реализация масштабирования
 
 var SCALE = {
@@ -297,6 +219,9 @@ var scaleControlSmaller = imgUpload.querySelector('.scale__control--smaller');
 var scaleControlBigger = imgUpload.querySelector('.scale__control--bigger');
 var scaleControlValue = imgUpload.querySelector('.scale__control--value');
 var scale = imgUpload.querySelector('.scale');
+
+var imgUploadPreview = imgUpload.querySelector('.img-upload__preview');
+var effectsList = imgUpload.querySelector('.effects__list');
 
 // функция устанавливает значение масштаба
 var setScaleControlValue = function (value) {
@@ -342,3 +267,116 @@ var onScaleControlClick = function (evt) {
 };
 
 scale.addEventListener('click', onScaleControlClick);
+
+
+// реализация смены эффектов
+
+var effectLevel = imgUpload.querySelector('.effect-level');
+var effectLevelPin = effectLevel.querySelector('.effect-level__pin');
+var effectLevelDepth = effectLevel.querySelector('.effect-level__depth');
+var effectLevelLine = effectLevel.querySelector('.effect-level__line');
+var effectLevelValue = effectLevel.querySelector('.effect-level__value');
+
+// функция сброса уровня эффекта, на уровень по умолчанию
+var resetEffectLevel = function (defaultEffectLevel) {
+  effectLevelPin.style.left = defaultEffectLevel + '%';
+  effectLevelDepth.style.width = defaultEffectLevel + '%';
+};
+
+resetEffectLevel(DEFAULT_EFFECT_LEVEL);
+
+// функция добавляет целевому элементу задаваемый класс
+// elemTarget - целевой элемент
+// extraClass - задаваемый класс
+var addElemntEffect = function (elemTarget, extraClass) {
+  elemTarget.classList = ''; // обнуляем класс лист
+  elemTarget.classList.add(UPLOADED_IMG_DEFAULT_CLASS); // добавляем класс по умолчанию
+  elemTarget.classList.add(extraClass); // добавляем заданный класс
+};
+
+// функция обработчик
+var onEffectsItemClick = function (evt) {
+
+  effectLevel.classList.remove('visually-hidden'); // показываем слайдер, который скрываем если фото без эффекта
+
+  if (evt.target.classList.contains('effects__preview')) {
+    var extraClass = evt.target.classList.item(1); // забираем нужный класс у элемента по которому кликаем
+    addElemntEffect(imgUploadPreview, extraClass);
+    resetEffectLevel(DEFAULT_EFFECT_LEVEL);
+  }
+
+  // если выбрали фото без эффекта, скроем слайдер
+  if (imgUploadPreview.classList.contains('effects__preview--none')) {
+    effectLevel.classList.add('visually-hidden');
+  }
+
+  imgUploadPreview.style.filter = ''; // при каждом переключении сбрасываем фильтр на фото
+};
+
+effectsList.addEventListener('click', onEffectsItemClick);
+
+
+// реализация насыщенности эффекта
+
+// функция изменяет стили для изменения уровня эффекта
+// depthOfEffect глубина эффекта от 0 до 100
+// effectOnImg - наложенный на элемент эффект
+// targetElement - целевой элемент с которым работаем
+
+var changeEffectLevel = function (depthOfEffect, effectOnImg, targetElement) {
+  switch (effectOnImg) {
+    case 'effects__preview--chrome':
+      targetElement.style.filter = 'grayscale(+' + 0.01 * depthOfEffect + ')';
+      break;
+    case 'effects__preview--sepia':
+      targetElement.style.filter = 'sepia(+' + 0.01 * depthOfEffect + ')';
+      break;
+    case 'effects__preview--marvin':
+      targetElement.style.filter = 'invert(' + depthOfEffect + '%)';
+      break;
+    case 'effects__preview--phobos':
+      targetElement.style.filter = 'blur(+' + 3 / 100 * depthOfEffect + 'px)';
+      break;
+    case 'effects__preview--heat':
+      targetElement.style.filter = 'brightness(+' + ((depthOfEffect * 0.01 * (3 - 1)) + 1) + ')';
+      break;
+  }
+};
+
+
+effectLevelPin.addEventListener('mousedown', function (evt) {
+  // все перемещения слайдера будем считать в процентах,
+  // для этого найдем масштбаный коэффициент
+  // получим ширину слайдера и соотнесем ее к 100%
+  var scaleFactor = effectLevelLine.offsetWidth / 100; // получаем ширину
+  var startX = evt.clientX;
+  var onMouseMove = function (moveEvt) {
+
+    var shift = (startX - moveEvt.clientX);
+    startX = moveEvt.clientX;
+    var currentPosition = effectLevelPin.offsetLeft;
+    var sliderPosition = (currentPosition - shift) / scaleFactor;
+
+    if (sliderPosition < 0) {
+      sliderPosition = 0;
+    }
+
+    if (sliderPosition > 100) {
+      sliderPosition = 100;
+    }
+
+    effectLevelPin.style.left = sliderPosition + '%';
+    effectLevelDepth.style.width = sliderPosition + '%';
+    effectLevelValue.value = sliderPosition;
+
+    changeEffectLevel(sliderPosition, imgUploadPreview.classList.item(1), imgUploadPreview);
+  };
+
+  var onMouseUp = function () {
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
